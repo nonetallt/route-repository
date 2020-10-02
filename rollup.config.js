48cf6f11ad02482405550041037aa49985542cdd
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2020-10-02 14:21:36 +0000

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,12 @@ import buble from 'rollup-plugin-buble';
 
 export default
 {
-    entry: 'src/LaravelRoutes.js',
-    dest: 'dist/laravel-js-routes.js',
-    format: 'cjs',
-    plugins: [buble()],
-    exports: 'default'
+  entry: 'src/LaravelRoutes.js',
+  dest: 'dist/laravel-js-routes.js',
+  format: 'cjs',
+  plugins: [buble()],
+  exports: 'default',
+  output: {
+    extend: true
+  }
 }
