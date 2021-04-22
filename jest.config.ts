1c775f3226d7f783d0987b3dd80f5f8f2336959f
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2021-04-22 18:10:59 +0300

    Use ts-jest instead of babel for testing

--- jest.config.ts
+++ jest.config.ts
@@ -60,10 +60,15 @@ export default {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: {
+      "ts-jest": {
+          // Should improve performance, see: https://github.com/kulshekhar/ts-jest/issues/259
+          isolatedModules: true
+      }
+  },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-  // maxWorkers: "50%",
+  maxWorkers: 1, // Seems to increase performance, see: https://github.com/kulshekhar/ts-jest/issues/259
 
   // An array of directory names to be searched recursively up from the requiring module's location
   // moduleDirectories: [
@@ -93,7 +98,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
