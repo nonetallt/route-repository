a025b5af95e69e239e66aebb1cc1a1a00b6cf375
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2021-04-29 21:30:04 +0300

    Rename HttpVerb

---
+++ src/HttpRequestMethod.ts
@@ -0,0 +1,18 @@
+/**
+ * A simple enum that contains valid HTTP methods
+ *
+ */
+enum HttpRequestMethod
+{
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE
+}
+
+export default HttpRequestMethod
