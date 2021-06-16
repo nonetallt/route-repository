a2605ba4a26124ca127486a0d1e2c156ce5db0ea
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2021-06-16 21:03:48 +0300

    Add bindUrl method and docs to applyPrefix

--- src/Route.ts
+++ src/Route.ts
@@ -13,8 +13,22 @@ export default class Route
         this.url = url
     }
 
+    /**
+     * Apply a given prefix to the route url
+     *
+     */
     applyPrefix(prefix: string)
     {
         this.url = prefix + this.url
+    }
+
+    /**
+     * Bind given parameter values to the route url parameter placeholders
+     *
+     */
+    bindUrl(values : string | Array<string> | object) : string
+    {
+
+        return this.url
     }
 }
