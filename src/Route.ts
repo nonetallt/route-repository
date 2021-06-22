7b27450fe63b9463beea3bd66803a27c6ac63c4e
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2021-06-22 22:38:45 +0300

    Use Url class in Route, add TODO

--- src/Route.ts
+++ src/Route.ts
@@ -1,16 +1,17 @@
 import RequestMethod from './RequestMethod';
+import Url from './Url'
 
 export default class Route
 {
     name: string
     method: RequestMethod
-    url: string
+    url: Url
 
-    constructor(name: string, method: RequestMethod, url: string)
+    constructor(name: string, method: RequestMethod, url: string | Url)
     {
         this.name = name
         this.method = method
-        this.url = url
+        this.url = typeof url === 'string' ? new Url(url) : url
     }
 
     /**
@@ -19,7 +20,7 @@ export default class Route
      */
     applyPrefix(prefix: string)
     {
-        this.url = prefix + this.url
+        this.url.content = prefix + this.url.content
     }
 
     /**
@@ -29,6 +30,9 @@ export default class Route
     bindUrl(values : string | Array<string> | object) : string
     {
 
-        return this.url
+        /* TODO: try catch wrap binding exception, invoke new one that exposes route name and url in error message */
+
+
+        return this.url.content
     }
 }
