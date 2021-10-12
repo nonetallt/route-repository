ae8412137b8d453c901d1bcc04893598fa06d0e0
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2021-10-12 19:52:45 +0300

    Remove redudant initializer and imports

--- src/config/RouteRegistrarConfiguration.ts
+++ src/config/RouteRegistrarConfiguration.ts
@@ -1,19 +1,22 @@
 import ConfigurationInterface from '../contract/RouteRegistrarConfigurationInterface'
-import UriConfiguration from './UriConfiguration'
 import UriConfigurationInterface from '../contract/UriConfigurationInterface'
-import BaseUriConfiguration from './BaseUriConfiguration'
 import BaseUriConfigurationInterface from '../contract/BaseUriConfigurationInterface'
 
 export default class RouteRegistrarConfiguration implements ConfigurationInterface
 {
     readonly extra: object
     readonly uris: UriConfigurationInterface
+
+    /**
+     * The reason this defaults to null instead of {} like other interfaces is because it has required properties.
+     *
+     */
     readonly baseUri: BaseUriConfigurationInterface | null
 
     constructor(config: ConfigurationInterface = {})
     {
         this.extra = {}
-        this.uris = new UriConfiguration()
+        this.uris = {}
         this.baseUri = null
 
         Object.assign(this, config);
