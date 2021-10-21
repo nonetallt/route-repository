d37dd91edec5ab0e478f75487b92edac58be7ef8
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2021-10-21 16:38:10 +0300

    Document applyMiddleware

--- src/contract/RouteMiddlewareInterface.ts
+++ src/contract/RouteMiddlewareInterface.ts
@@ -2,5 +2,11 @@ import Route from '../Route'
 
 export default interface RouteMiddlewareInterface
 {
+    /**
+     * @throws RegistrationError
+     *
+     * Apply the middleware to a given route.
+     *
+     */
     applyMiddleware: (route: Route) => Route
 }
